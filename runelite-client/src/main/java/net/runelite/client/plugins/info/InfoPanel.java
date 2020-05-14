fd1f1172eb22eb1bbca170a3b4ba209ebc332e20
Andrew McAdams <andmcadams@gmail.com>
2020-05-13 19:16:36 -0500

    Move everything into an invokelater call so it runs on the client thread

--- runelite-client/src/main/java/net/runelite/client/plugins/info/InfoPanel.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/info/InfoPanel.java
@@ -54,6 +54,7 @@ import net.runelite.api.Quest;
 import net.runelite.api.Skill;
 import net.runelite.api.VarPlayer;
 import net.runelite.api.vars.SlayerUnlock;
+import net.runelite.client.callback.ClientThread;
 import net.runelite.client.events.SessionClose;
 import net.runelite.client.events.SessionOpen;
 import net.runelite.client.RuneLiteProperties;
@@ -83,6 +84,9 @@ public class InfoPanel extends PluginPanel
 	private final JRichTextPane emailLabel = new JRichTextPane();
 	private JPanel syncPanel;
 	private JPanel actionsContainer;
+
+	@Inject
+	private ClientThread clientThread;
 
 	@Inject
 	@Nullable
@@ -211,50 +215,56 @@ public class InfoPanel extends PluginPanel
 		if (client != null && client.getLocalPlayer() != null)
 		{
 
-			HashMap<String, HashMap> output_dict = new HashMap<>();
 
-			//Quests
-			HashMap<Integer, String> quest_dict = new HashMap<>();
-			for (Quest quest : Quest.values())
-			{
-				quest_dict.put(quest.getId(), quest.getState(client).name());
-			}
-			output_dict.put("Quests", quest_dict);
+			clientThread.invokeLater(() ->
+				{
+					final HashMap<String, HashMap> output_dict = new HashMap<>();
+					final HashMap<Integer, String> quest_dict = new HashMap<>();
+					final HashMap<String, Integer> skill_dict = new HashMap<>();
+					final HashMap<String, Integer> favour_dict = new HashMap<>();
+					final HashMap<SlayerUnlock, Boolean> slayer_dict = new HashMap<>();
+					final HashMap<String, Integer> other_dict = new HashMap<String, Integer>();
 
-			//Skills
-			HashMap<String, Integer> skill_dict = new HashMap<>();
-			for (Skill skill : Skill.values())
-			{
-				skill_dict.put(skill.getName(), client.getRealSkillLevel(skill));
-			}
-			output_dict.put("Skills", skill_dict);
+					//Quests
+					for (Quest quest : Quest.values())
+					{
+						quest_dict.put(quest.getId(), quest.getState(client).name());
+					}
 
-			//Favour
-			HashMap<String, Integer> favour_dict = new HashMap<>();
-			for (Favour favour : Favour.values())
-			{
-				favour_dict.put(favour.getName(), client.getVar(favour.getVarbit()));
-			}
-			output_dict.put("Favour", favour_dict);
+					//Skills
+					for (Skill skill : Skill.values())
+					{
+						skill_dict.put(skill.getName(), client.getRealSkillLevel(skill));
+					}
 
-			//Slayer
-			HashMap<SlayerUnlock, Boolean> slayer_dict = new HashMap<>();
-			for (SlayerUnlock slayerunlock : SlayerUnlock.values())
-			{
-				slayer_dict.put(slayerunlock, slayerunlock.isEnabled(client));
-			}
-			output_dict.put("SlayerUnlocks", slayer_dict);
+					//Favour
+					for (Favour favour : Favour.values())
+					{
+						favour_dict.put(favour.getName(), client.getVar(favour.getVarbit()));
+					}
 
-			//Other
-			HashMap<String, Integer> other_dict = new HashMap<String, Integer>();
-			other_dict.put("QP", client.getVar(VarPlayer.QUEST_POINTS));
-			other_dict.put("Combat", client.getLocalPlayer().getCombatLevel());
-			other_dict.put("Iron", client.getAccountType().isIronman() ? 1 : 0);
-			output_dict.put("Other", other_dict);
+					//Slayer
+					for (SlayerUnlock slayerunlock : SlayerUnlock.values())
+					{
+						slayer_dict.put(slayerunlock, slayerunlock.isEnabled(client));
+					}
 
-			String output = gson.toJson(output_dict);
-			StringSelection stringselection = new StringSelection(output);
-			Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringselection, null);
+
+					//Other
+					other_dict.put("QP", client.getVar(VarPlayer.QUEST_POINTS));
+					other_dict.put("Combat", client.getLocalPlayer().getCombatLevel());
+					other_dict.put("Iron", client.getAccountType().isIronman() ? 1 : 0);
+					
+					output_dict.put("Quests", quest_dict);
+					output_dict.put("Skills", skill_dict);
+					output_dict.put("Favour", favour_dict);
+					output_dict.put("SlayerUnlocks", slayer_dict);
+					output_dict.put("Other", other_dict);
+
+					String output = gson.toJson(output_dict);
+					StringSelection stringselection = new StringSelection(output);
+					Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringselection, null);
+				});
 		}
 	}
 
